584d8fb5ff321aba4f1f22a4e544bd4b82f25006
Yehee <iixye827@gmail.com>
2026-02-16 19:16:42 +0800

    Update auth-check.js

--- auth-check.js
+++ auth-check.js
@@ -6,9 +6,7 @@
     // clear the timestamp and redirect to the login page.
     if (!authTimestamp || (Date.now() - parseInt(authTimestamp, 10) > sevenDaysInMs)) {
         localStorage.removeItem('authTimestamp');
-        // Only redirect if we are not already on the login page.
-        if (window.location.pathname.endsWith('index.html') === false && window.location.pathname.endsWith('/') === false) {
-            window.location.href = 'index.html';
-        }
+        // Redirect to the login page, which is index.html
+        window.location.href = 'index.html';
     }
 })();
